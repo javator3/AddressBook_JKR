e581409319823e1463703af84869311dec0c827a
Joanna Krzyżanowska <joanna.krzyzanowska@o2.pl>
2019-02-16 11:46:26 +0100

    "NewPerson.fxml added"

---
+++ src/main/java/pl/sda/addressbook/AddressBook.java
@@ -0,0 +1,14 @@
+package pl.sda.addressbook;
+
+public class AddressBook {
+
+    private String name;
+    private String lastname;
+    private String street;
+    private String city;
+    private String postalcode;
+    private String telephone;
+
+}
+
+
