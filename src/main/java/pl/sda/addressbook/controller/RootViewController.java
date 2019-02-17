b71b6063c7d3737987c286a15f7a1f714036bea8
Joanna Krzyżanowska <joanna.krzyzanowska@o2.pl>
2019-02-17 10:06:22 +0100

    method saveToJason added

--- src/main/java/pl/sda/addressbook/controller/RootViewController.java
+++ src/main/java/pl/sda/addressbook/controller/RootViewController.java
@@ -1,14 +1,18 @@
 package pl.sda.addressbook.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import pl.sda.addressbook.model.Person;
 import pl.sda.addressbook.view.PersonView;
 
+import java.io.File;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -41,6 +45,9 @@ public class RootViewController implements Initializable {
     @FXML
     private TableColumn<Person, String> lastName;
 
+    @FXML
+    private Button saveToJason;
+
     private PersonView personView;
 
     public void setPersonView(PersonView personView){
@@ -63,4 +70,19 @@ public class RootViewController implements Initializable {
     public void loadNewPerson(ActionEvent actionEvent) {
         personView.loadNewPersonView();
     }
+
+    public void saveToJason(ActionEvent actionEvent){
+
+        ObjectMapper mapper = new ObjectMapper();
+        File file = new File("personList");
+
+        try {
+            mapper.writeValue(file,personView.getPersonList() );
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
 }
